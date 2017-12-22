927cf8124765256bdf9d92331ffba833379c5925
joxar <mfpirlo@yahoo.co.jp>
2017-12-23 03:53:47 +0900

    a

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='face',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='joxar',
+      author_email='a@test.com',
+      url='https://voice-pkg-meta-colascione.cfapps.io/',
+      packages=['server/live-server']
+     )
